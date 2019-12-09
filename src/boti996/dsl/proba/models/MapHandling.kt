68901cc1c945cf7a71cb49e81652ec950164de77
boti996 <dobai.botond@gmail.com>
2019-12-09 03:45:39 +0100

    handle current level in Game

--- src/boti996/dsl/proba/models/MapHandling.kt
+++ src/boti996/dsl/proba/models/MapHandling.kt
@@ -30,7 +30,20 @@ data class Game(val initialMoney: Int,
     fun getShop() = shop
     fun setShop(newShop : Shop) { shop = newShop }
 
+    private var currentLevelIdx = 0
+
     fun getCurrentLevel() : Level {
-        TODO()
+        return levels[currentLevelIdx]
+    }
+
+    fun setCurrentLevel(levelIdx : Int, isSkipping: Boolean = false) : Boolean {
+        if (levels.size <= levelIdx) return false   // non-existent level
+        if (isSkipping) {
+            if (levelIdx - currentLevelIdx > maxSkippableLevels) return false   // too much skipped level
+            for (idx in currentLevelIdx until levelIdx)
+                if (!levels[idx].isSkippable) return false  // non-skippable level
+        }
+        currentLevelIdx = levelIdx  // otherwise
+        return true
     }
 }
